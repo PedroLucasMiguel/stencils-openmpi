d21a5fa5ee4985da4a31cc7b6dbb1f4802e61355
PedroLucasMiguel <pedro.lu.miguel@gmail.com>
2022-08-12 10:04:57 -0300

    Removing MPI local enviroment because it was kinda dumb

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # stencils-openmpi
 A stencil program using Open MPI
-
-# To activate the enviroment run: 
-```shell
-export PATH=$PATH:$(pwd)/project-openmpi/bin
-```
--- src/main.c
+++ src/main.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <mpi.h>
 
-int main() {
-    printf("Hello World!\n");
+#define ITERATION_COUNT 1000
+#define N_CHANNELS 3
 
-    return 0;
+void Init_matrix(double*** matrix, int rows, int columns) {
+
+    int i = 0;
+    int j = 0;
+
+    matrix = (double***)malloc(rows * sizeof(double**));
+    
+    for(i = 0; i < rows; i++) {
+        matrix[i] = (double**)malloc(columns * sizeof(double*));
+        
+        for(j = 0; j < columns; j++) {
+            matrix[i][j] = (double*)malloc(N_CHANNELS * sizeof(double)); 
+        }
+    }
+
+}
+
+
+int main(int argc, char** argv) {
+
+    MPI_Comm newComm; // Recebera o novo "universo"
+    
+    // Matrizes
+    double*** red;
+    double*** black;
+    double*** final;
+
+    int nProcesses; // Recebe a quantidade de processos que fazem parte do Comm
+    int processID; // Rebe o ID do processo
+
+    MPI_Init (&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
+
+    printf("%d\n", nProcesses);
+
+    MPI_Finalize();
 }
