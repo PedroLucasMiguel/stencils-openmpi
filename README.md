88a8bf41653fb0343005ec9812eeae23ee7a35fc
PedroLucasMiguel <pedro.lu.miguel@gmail.com>
2022-08-12 10:40:54 -0300

    Finishing the "base" for the work.
    Right now we only have the basic setup to start the process.
    
    We still need to figure out how to send and recieve each
    message correctly, but, at the end of the day, that's a
    start...

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stencils-openmpi
 A stencil program using Open MPI
+
+# To compile:
+```shell
+mpicc -o out main.c && mpirun -np <n_processes> out
+```
--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <mpi.h>
 
 #define ITERATION_COUNT 1000
 #define N_CHANNELS 3
+#define N_DIM 1
+#define MAX_PROCESS 16
+#define MANAGER_ID 0
+
+#define FAKE_IMAGE_ROWS 256
+#define FAKE_IMAGE_COLUMNS 256
 
 void Init_matrix(double*** matrix, int rows, int columns) {
 
@@ -23,24 +28,42 @@ void Init_matrix(double*** matrix, int rows, int columns) {
 
 }
 
-
 int main(int argc, char** argv) {
 
     MPI_Comm newComm; // Recebera o novo "universo"
     
     // Matrizes
-    double*** red;
-    double*** black;
-    double*** final;
+    double*** matrixToProcess;
+    double*** finalMatrix;
 
-    int nProcesses; // Recebe a quantidade de processos que fazem parte do Comm
-    int processID; // Rebe o ID do processo
+    int nProcesses; // Quantidade de processos que fazem parte do Cartesiano
+    int processRank; // Rank do processo
+    int dims; // Quantidade de dimensões para o novo Cartesiano
+    int periods = 0; // Indica se o novo Cartesiano será periódico ou não (0 e N se conectam)
+    int reorder = 1; // Indica se os processos no novo Cartesiano podem ser reordenados
+    int coords; // Coordenada do processo dentro do Cartesiano
+    int upProcess; // Processo superior
+    int downProcess; // Processo inferior
 
     MPI_Init (&argc, &argv);
 
+    // Retorna o número de processos (definido pelo parâmetro -np na execução)
     MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
 
-    printf("%d\n", nProcesses);
+    // Define o valor de dimensão X do novo cartesiano baseado no número de processos
+    dims = nProcesses;
+
+    // Cria um novo cartesiano (newCom) com dimensão "N_DIM x dims" (1 x N)
+    MPI_Cart_create(MPI_COMM_WORLD, N_DIM, &dims, &periods, reorder, &newComm);
+
+    // Armazena o rank do processo em "processRank"
+    MPI_Comm_rank(newComm, &processRank);
+
+    // Recebe a localização desse processo dentro do Cartesiano e armazena em "coords"
+    MPI_Cart_get(newComm, MAX_PROCESS, &dims, &periods, &coords);
+
+    // Recebe a localização do processo superior (se existir) e inferior (se existir)
+    MPI_Cart_shift(newComm, 0, 1, &upProcess, &downProcess);
 
     MPI_Finalize();
 }
